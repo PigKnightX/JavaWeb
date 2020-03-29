7ee25de0a042b4e655ad92f10856830c62548bb0
PigKnightX <PigKnightX@163.com>
2020-03-30 01:25:21 +0800

    day_04

--- dataSource_jdbcTemplate/src/cn/pigknight/datasource/c3p0/c3p0Demo2.java
+++ dataSource_jdbcTemplate/src/cn/pigknight/datasource/c3p0/c3p0Demo2.java
@@ -8,10 +8,37 @@ import java.sql.SQLException;
 
 public class c3p0Demo2 {
     public static void main(String[] args) throws SQLException {
-        //1.获取DataSource
+        //1.获取DataSource，使用默认配置
         DataSource ds = new ComboPooledDataSource();
-        //2.
-        Connection conn = ds.getConnection();
+        //1.1 获取DataSource，使用指定名称配置
+       // DataSource ds = new ComboPooledDataSource("otherc3p0");
+        //2.获取连接
+
+        for (int i = 1; i <= 11; i++) {
+            Connection conn = ds.getConnection();
+            System.out.println(i+":"+conn);
+
+            if (i==5){
+                conn.close();
+            }
+        }
 
     }
+
+    public static void testNamedConfig() throws SQLException {
+        //1.1 获取DataSource，使用指定名称配置
+        DataSource ds = new ComboPooledDataSource("otherc3p0");
+        //2.获取连接
+
+        for (int i = 1; i <= 11; i++) {
+            Connection conn = ds.getConnection();
+            System.out.println(i+":"+conn);
+
+            if (i==5){
+                conn.close();
+            }
+        }
+    }
+
+
 }
